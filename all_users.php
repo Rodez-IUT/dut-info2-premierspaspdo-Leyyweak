e2b716863415c2df538481eb3dc2d859c388ba17
Landry <landry.assemat@iut-rodez.fr>
2019-11-06 10:28:17 +0100

    a

---
+++ all_users.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<meta charset="UTF-8"/>
+<body>
+<table>
+	<tr>
+		<td>Id</td>
+		<td>Username</td>
+		<td>Email</td>
+		<td>Status</td>
+	</tr>
+<tr>
+<?php 
+$host = 'localhost';
+$db   = 'my-activities';
+$user = 'root';
+$pass = 'root';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+     throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$stmt = $pdo->query('SELECT * FROM users');
+while ($row = $stmt->fetch())
+{
+    echo $row['id'] . "\n";
+	echo $row['username'] . "\n";
+	echo $row['email'] . "\n";
+}
+
+$stmt = $pdo->query('SELECT username FROM users');
+while ($row = $stmt->fetch())
+{
+    echo $row['username'] . "\n";
+}
+
+$stmt = $pdo->query('SELECT email FROM users');
+while ($row = $stmt->fetch())
+{
+    echo $row['email'] . "\n";
+}
+
+$stmt = $pdo->query('SELECT status_id FROM users');
+while ($row = $stmt->fetch())
+{
+    echo $row['status_id'] . "\n";
+}
+
+
+?>
+</tr>
+</table>
+</body>
+</html>
