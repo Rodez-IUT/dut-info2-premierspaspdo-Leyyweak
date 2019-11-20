bb8e72c147de03f042ab943a8b598e49fec05429
Landry <landry.assemat@iut-rodez.fr>
2019-11-20 10:05:24 +0100

    fix #3.2 Moteur de recherche sur status et username

--- all_users.php
+++ all_users.php
@@ -1,45 +1,95 @@
-<!doctype html>
-<html>
-<meta charset="UTF-8"/>
-<body>
-<table>
-	<tr>
-		<td>Id</td>
-		<td>Username</td>
-		<td>Email</td>
-		<td>Status</td>
-	</tr>
-<?php 
-$host = 'localhost';
-$db   = 'my-activities';
-$user = 'root';
-$pass = 'root';
-$charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (PDOException $e) {
-     throw new PDOException($e->getMessage(), (int)$e->getCode());
-}
+<!DOCTYPE html>
+<hmtl lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Activité 2</title>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%;
+            }
+            th, td {
+                padding: 8px;
+                text-align: left;
+                border-bottom: 1px solid #ddd;
+            }
+    </style>
+    </head>
 
-$stmt = $pdo->query('SELECT * FROM users AS user JOIN status AS s ON status_id=s.id WHERE status="Active account" AND user.username LIKE "e%" ORDER BY username');
-while ($row = $stmt->fetch())
-{
-	
-    echo "<tr>";
-	echo "<td>".$row['id']."</td>";
-	echo "<td>".$row['username']."</td>";
-	echo "<td>".$row['email']."</td>";
-	echo "</tr>";
-}
+    <body>
+        <!-- Connexion à la BD -->
+        <?php
+        // Connexion à la BD
+            $host = 'localhost';
+            $db   = 'my_activities';
+            $user = 'root';
+            $pass = 'root';
+            $charset = 'utf8';
+            $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+            $options = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES   => false,
+            ];
+            try {
+                 $pdo = new PDO($dsn, $user, $pass, $options);
+            } catch (PDOException $e) {
+                 throw new PDOException($e->getMessage(), (int)$e->getCode());
+            }
+        ?>
 
+        <!-- Formulaire -->
+        <form method="post" action="all_users - 2.php" ID="formRecherche">
+            Première lettre du prénom : <input type="text" name="lettre"><br>
+            Status du compte :
+            <select name="status">
+                <option value="active">Active account</option>
+                <option value="waiting">Waiting for account validation</option>
+            </select>
+            <br/><input type="submit" value="Effectuer la recherche">
+        </form>
 
-?>
-</table>
-</body>
-</html>
+        <?php
+            if (isset($_POST['status']) && isset($_POST['lettre'])) {
+                // Vérifier qu'une seule lettre à été saisie
+                if (strlen($_POST['lettre']) == 1) {
+                    $lettre = $_POST['lettre'];
+                } else {
+                    echo "ERREUR";
+                    $lettre = "";
+                }
+                // Chercher l'ID du status
+                if (strcmp($_POST['status'], "active") == 0) {
+                    $status = 2;
+                } else {
+                    $status = 1;
+                }
+                // Préparation requête
+                $stmt = $pdo->query("SELECT users.id AS user_id, username, email, s.name AS status FROM users JOIN status s ON users.status_id = s.id WHERE s.id = $status AND username LIKE '$lettre%' ORDER BY username");
+            } else {
+                $stmt = $pdo->query("SELECT users.id AS user_id, username, email, s.name AS status FROM users JOIN status s ON users.status_id = s.id ORDER BY username");
+            }
+                // Entête tableau
+                echo"<h1>All users</h1>";
+                echo "<table border=\"1px\">";
+                echo "<tr>";
+                echo "<th>Id</th>";
+                echo "<th>Username</th>";
+                echo "<th>Email</th>";
+                echo "<th>Status</th>";
+                echo "</tr>";
+                // Récupération des données
+                while($row = $stmt->fetch()){
+                    // Récupération des données et affichage
+                    echo "<tr>";
+                    echo "<td>".$row['user_id']."</td>";
+                    echo "<td>".$row['username']."</td>";
+                    echo "<td>".$row['email']."</td>";
+                    echo "<td>".$row['status']."</td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+        ?>
+        </table>
+    </body>
+</hmtl>
